4f2ac8ce27c19a229ef9926d817e2248434735e8
Hien PH <hienduyph@gmail.com>
2018-03-24 18:03:38 +0700

    delete php laravel folder

--- ProgrammingLanguages/Php/beginner/cookie.php
+++
@@ -1,3 +0,0 @@
-<?php
-    // setcookie('q', 'tm', time() + 10);
-?>
--- ProgrammingLanguages/Php/beginner/header.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>learn php the hard way</title>
-</head>
-<body>
-<div>this is header</div>
