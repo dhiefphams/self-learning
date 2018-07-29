7b3ce8c5be86a988f4f22593357a2e5c89d09828
Hien PH <hienduyph@gmail.com>
2018-07-29 13:24:48 +0700

    feat: remove rails

--- Frameworks/rails/restapi/readme.md
+++
@@ -1,1 +0,0 @@
-https://youtu.be/QojnRc7SS9o
--- Frameworks/rails/starter/app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  # one user has many articles
-  has_many :articles
-  validates :password, presence: true, length: { minimum: 8}
-
-  validates :email, presence: true
-end
